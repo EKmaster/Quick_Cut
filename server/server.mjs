577079a464b4d035fffa03ef4d08b06f8d752caf
omarzaheer <omar.m.zaheer@gmail.com>
2024-07-28 12:21:37 +0300

    added password hashing

--- server/server.mjs
+++ server/server.mjs
@@ -4,21 +4,22 @@ import cors from "cors";
 import bodyParser from 'body-parser';
 import session from "express-session";
 import passport from "passport";
-import {customers} from "./mockUsers.mjs"
+import { customers } from "./mockUsers.mjs"
 import "./strategies/local-strategy.mjs";
 import mongoose from "mongoose";
 import MongoStore from "connect-mongo";
 import { User } from "./mongoose/schemas/user.mjs";
+import bcrypt from "bcrypt"
 
 const PORT = 8080;
 const app = express();
-mongoose.connect("mongodb+srv://omerkhan5002:3Nz0bihPwrbkcgps@cluster0.sd9uxwv.mongodb.net").then(()=> console.log("Connected to Database")).catch((err) => console.log(`Error: ${err}`));
+mongoose.connect("mongodb+srv://omerkhan5002:3Nz0bihPwrbkcgps@cluster0.sd9uxwv.mongodb.net").then(() => console.log("Connected to Database")).catch((err) => console.log(`Error: ${err}`));
 //3Nz0bihPwrbkcgps
 app.use(cors({
     origin: 'http://localhost:3000', // Update this to your frontend's URL
     credentials: true,
-  }));
-  
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -26,7 +27,7 @@ app.use(session({
     secret: "hello world",
     saveUninitialized: false,
     resave: false,
-    cookie : {
+    cookie: {
         maxAge: 60000 * 60,
     },
     store: MongoStore.create({
@@ -43,66 +44,68 @@ app.get("/", (req, res) => {
     console.log(req.session)
     console.log(req.session.id)
     req.session.visited = true
-    
-    res.status(201).send({"message": bookings})
+
+    res.status(201).send({ "message": bookings })
 });
 app.post("/api/auth/login", passport.authenticate("local"), (req, res) => {
-    
     res.sendStatus(200);
 })
 
 app.get("/api/auth/status", (req, res) => {
     req.session.visited = true
     console.log("Session ID:", req.session.id); // Check if session ID is present
-  console.log("User:", req.user); // Check if user is defined
-  console.log("Session:", req.session); // Check session data
+    console.log("User:", req.user); // Check if user is defined
+    console.log("Session:", req.session); // Check session data
 
     if (req.user) return res.sendStatus(200);
     console.log("yesss")
     return res.sendStatus(401);
 })
+
 app.post("/api/auth/logout", (req, res) => {
     if (!req.user) return res.sendStatus(401);
     req.logout((err) => {
-    if (err) return res.sendStatus(400);
+        if (err) return res.sendStatus(400);
         res.sendStatus(200);
+    })
 })
-})
+
 app.post("/api/auth/signup", async (req, res) => {
-    const {body} = req;
+    const { body } = req;
+    const password = body.password
+    const salt = bcrypt.genSaltSync(10)
+    body.password = bcrypt.hashSync(password, salt)
+
     const newUser = new User(body)
+
     try {
         const savedUser = await newUser.save()
-        const data = {
-            email: req.body.email, password: req.body.password, firstName: req.body.firstName, lastName: req.body.lastName
-        }
-    
-        req.login(savedUser, function(err) {
-            if (err){
+        req.login(savedUser, function (err) {
+            if (err) {
                 console.log(err)
                 return res.status(201).send(savedUser)
             }
             res.sendStatus(200);
-            
+
         })
-        
+
     } catch (err) {
         console.log(err)
         return res.sendStatus(400)
     }
 
-
 })
+
 app.post("/api/book", (req, res) => {
     const name = req.body.name
     const city = req.body.city
     const haircut = req.body.haircut
     const timing = req.body.timing
     const data = {
-        "name" : name,
-        "city" : city,
-        "timing" : timing,
-        "haircut" : haircut
+        "name": name,
+        "city": city,
+        "timing": timing,
+        "haircut": haircut
     }
     bookings.push(data)
     res.send(`Booking completed for ${name}`)
--- server/strategies/local-strategy.mjs
+++ server/strategies/local-strategy.mjs
@@ -1,7 +1,8 @@
 import password from "passport"
-import {Strategy} from "passport-local"
-import {customers} from "../mockUsers.mjs"
+import { Strategy } from "passport-local"
+import { customers } from "../mockUsers.mjs"
 import { User } from "../mongoose/schemas/user.mjs";
+import bcrypt from "bcrypt"
 
 password.serializeUser((user, done) => {
     console.log("Insde Serializer")
@@ -10,14 +11,14 @@ password.serializeUser((user, done) => {
 
 })
 
-password.deserializeUser( async (id, done) => {
+password.deserializeUser(async (id, done) => {
     console.log(`Inside Deserializer with ID: ${id}`);
-    
+
     try {
         const findUser = await User.findById(id);
 
         if (!findUser) throw new Error("User not found")
-        
+
         done(null, findUser)
     }
     catch (err) {
@@ -28,17 +29,15 @@ password.deserializeUser( async (id, done) => {
 
 
 export default password.use(
-    new Strategy({usernameField: "email"}, async (email, password, done) => {
+    new Strategy({ usernameField: "email" }, async (email, password, done) => {
         console.log(email)
         console.log(password)
         try {
             const findUser = await User.findOne({ email });
 
             if (!findUser) throw new Error("User not found")
-                if (findUser.password !== password)
-                    throw new Error("Bad Credentials");
-    
-            
+            if (!bcrypt.compareSync(password, findUser.password)) throw new Error("Bad Credentials")
+
             done(null, findUser)
         }
         catch (err) {
