893bc33d346ab52b76752a87139c216d01dd0e65
omarzaheer <omar.m.zaheer@gmail.com>
2024-08-03 00:04:42 +0300

    fixed minor bug in logout frontend

--- client/app/logout/page.tsx
+++ client/app/logout/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { getCsrfToken } from '../utils/csrfToken';
 
 export default function LogoutPage() {
     const router = useRouter()
+
     async function handleClick() {
+
+        const csrfToken = await getCsrfToken();
         const response = await fetch('http://localhost:8080/api/auth/logout', {
             method: 'POST',
             credentials: 'include',
+            headers: {
+                'X-CSRF-Token': csrfToken
+            }
         })
         if (response.ok){
             router.push("/")
