17712de5d7e6ec5463a3ab6b703e13531d285f6b
omarzaheer <omar.m.zaheer@gmail.com>
2024-08-15 23:29:17 +0300

    minor improvement to search dropbox styling

--- client/app/components/dropdownSearch.tsx
+++ client/app/components/dropdownSearch.tsx
@@ -13,9 +13,18 @@ interface DropdownSearchProps {
 function DropdownSearch({ onSearchChange, optionsList, selectOption }: DropdownSearchProps) {
     const [inputAvailabe, setInputAvailable] = useState(false)
     const [nullSelection, setNullSelection] = useState(true)
+    const [dropdownOpen, setDropdownOpen] = useState(false)
 
     useEffect(() => {
         setInputAvailable(!!document.getElementById('dropdown-search'));
+    }, [])
+
+    useEffect(() => {
+        if (document.getElementById("dropdown-item")){
+            setDropdownOpen(true)
+        }else{
+            setDropdownOpen(false)
+        }
     })
 
     async function selectOptionHandler(description: string, id: string | number) {
@@ -28,7 +37,9 @@ function DropdownSearch({ onSearchChange, optionsList, selectOption }: DropdownS
 
     return (
         <div style={{ position: "relative" }}>
-            <div className={styles.inputForm}>
+            <div className={styles.inputForm} style={{
+                borderRadius: dropdownOpen ? "10px 10px 0 0" : "10px"
+            }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width='24' height='24' viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10" /><path d="M16.2 7.8l-2 6.3-6.4 2.1 2-6.3z" /></svg>
                 <input
                     id="dropdown-search"
@@ -39,20 +50,18 @@ function DropdownSearch({ onSearchChange, optionsList, selectOption }: DropdownS
                         if (!nullSelection){
                             setNullSelection(true)
                         }
-                        {/*function call every time the saerch in the drop down is edited*/ }
                         onSearchChange(e.target.value)
                     }}>
                 </input>
             </div>
 
 
-            {/* */}
             {
                 nullSelection && optionsList.length  !== 0 ? (
                     <ul className={styles.dropdownList}>
                         {
                             optionsList.map(({ description, id }) => (
-                                <li key={id}>
+                                <li id="dropdown-item" key={id}>
                                     <button type="button" onClick={async () => {
                                         await selectOptionHandler(description, id)
                                     }}>
@@ -68,33 +77,6 @@ function DropdownSearch({ onSearchChange, optionsList, selectOption }: DropdownS
                     </ul>
                 ) : (null)
             }
-
-            {/*   */ }
-
-            {/*
-            {optionsList.length !== 0 ? (
-                <ul className={styles.dropdownList}>
-                    {
-                        optionsList.map(({ description, id }) => (
-                            <li key={id}>
-                                <button type="button" onClick={async () => {
-                                    await selectOptionHandler(description, id)
-                                }}>
-                                    {description}
-                                </button>
-                            </li>
-                        ))
-                    }
-                </ul>
-            ) : (inputAvailabe && !document.getElementById("dropdown-search")) ? (
-                (null)
-            ) : inputAvailabe && (document.getElementById("dropdown-search") as HTMLInputElement).value !== "" ? (
-                <ul className={styles.dropdownList}>
-                    <li>No results found</li>
-                </ul>
-            ) : (null)
-            }
-            */}
 
         </div>
     )
